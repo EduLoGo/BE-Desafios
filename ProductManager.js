beef27fe9c0f0f5e04bdc7114ad9305d8b2decbd
EduLoGo <edulogo.dev@gmail.com>
2023-10-24 18:40:45 -0300

    Desafio - Manejo de Archivo

--- ProductManager.js
+++ ProductManager.js
@@ -1,10 +1,30 @@
+const fs = require("fs");
+
 class ProductManager {
-  id = 1;
-  constructor() {
-    this.products = [];
+  id = 0;
+  constructor(path) {
+    this.path = path;
   }
+  // Cargar Archivo
+  loadData() {
+    try {
+      return JSON.parse(fs.readFileSync(this.path, "utf-8"));
+    } catch (error) {
+      return [];
+    }
+  }
+  // Guardar en Archivo
+  saveData(data) {
+    try {
+      fs.writeFileSync(this.path, JSON.stringify(data));
+    } catch (error) {
+      return console.log(`Couldn't save data`);
+    }
+  }
+  // Agregar Producto
   addProduct(newProduct) {
-    let checkCode = this.products.some((elem) => elem.code === newProduct.code);
+    const dataDB = this.loadData();
+    let checkCode = dataDB.some((elem) => elem.code === newProduct.code);
     if (checkCode) {
       return console.log(`Code already exist`);
     } else if (
@@ -17,19 +37,53 @@ class ProductManager {
     ) {
       return console.log(`Missing data`);
     } else {
+      if (dataDB.length === 0) {
+        this.id = 1;
+      } else {
+        this.id = dataDB[dataDB.length - 1].id + 1;
+      }
       let productUpdated = { id: this.id, ...newProduct };
-      this.products.push(productUpdated);
       this.id++;
+      dataDB.push(productUpdated);
+      this.saveData(dataDB);
       return console.log(`Product added successfully`);
     }
   }
-  getProduct() {
-    console.log(this.products);
+  // Obtener todos los Productos
+  getProducts() {
+    const dataDB = this.loadData();
+    return console.log(dataDB);
   }
+  // Obtener Producto por ID
   getProductByID(id) {
-    let existProduct = this.products.find((elem) => elem.id === id);
+    const dataDB = this.loadData();
+    let existProduct = dataDB.find((elem) => elem.id === id);
     if (existProduct) {
       return console.log(existProduct);
+    } else {
+      return console.log(`Product ID ${id} not found!`);
+    }
+  }
+  // Actualizar Producto
+  updateProduct(id, newInfo) {
+    const dataDB = this.loadData();
+    let index = dataDB.findIndex((elem) => elem.id === id);
+    if (index !== -1) {
+      dataDB[index] = { ...dataDB[index], ...newInfo };
+      this.saveData(dataDB);
+      return console.log(`Product updated successfully`);
+    } else {
+      return console.log(`Product ID ${id} not found!`);
+    }
+  }
+  // Eliminar Producto
+  deleteProduct(id) {
+    const dataDB = this.loadData();
+    let index = dataDB.findIndex((elem) => elem.id === id);
+    if (index !== -1) {
+      dataDB.splice(index, 1);
+      this.saveData(dataDB);
+      return console.log(`Product deleted successfully`);
     } else {
       return console.log(`Product ID ${id} not found!`);
     }
@@ -44,20 +98,32 @@ const productA = {
   code: "abc123",
   stock: 25,
 };
-
 const productB = {
   title: "Producto Prueba 2",
-  description: "Este es otro producto de prueba",
-  price: 800,
+  description: "Este es un producto prueba",
+  price: 300,
   thumbnail: "Sin imagen",
   code: "abc987",
+  stock: 35,
+};
+
+const newData = {
+  title: "Nuevo Nombre Producto Prueba",
+  price: 500,
   stock: 50,
 };
 
-//Testing Process
-const productManager = new ProductManager();
-productManager.getProduct();
-productManager.addProduct(productA);
-productManager.getProduct();
-productManager.addProduct(productA);
-productManager.getProductByID(2);
+// Proceso de Testing
+
+// const appManager = new ProductManager("./productsDB.json");
+// appManager.getProducts();
+// appManager.addProduct(productA);
+// appManager.getProducts();
+// appManager.addProduct(productB);
+// appManager.getProducts();
+// appManager.getProductByID(2)
+// appManager.getProductByID(9)
+// appManager.updateProduct(2,newData)
+// appManager.updateProduct(9, newData)
+// appManager.deleteProduct(9)
+// appManager.deleteProduct(3)
